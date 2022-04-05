9e712b4f1af0cae9f6a4a08b32fea466b3bc946c
lautaro volcan <lautaroferrandi@gmail.com>
2022-04-05 19:34:09 -0300

    routingmaso

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetail/Ite
 
 // Routes
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/items" element={ <ItemListContainer greetings='SoxCommerce'/>} />
+          <Route path="/" element={ <ItemListContainer greetings='SoxCommerce'/>} />
           <Route path="/Item1" element={<ItemDetailContainer/> }/>
         </Routes>
       </BrowserRouter>
